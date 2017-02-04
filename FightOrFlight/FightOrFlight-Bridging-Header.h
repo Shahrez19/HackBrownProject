c8b3ca09b1fe1a7fd48f8c137fd86cbb9c07c5a6
fortunac <cfortuna@bu.edu>
2017-02-04 15:10:49 -0500

    Add Myo framework

---
+++ FightOrFlight/FightOrFlight-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  FightOrFlight-Bridging-Header.h
+//  FightOrFlight
+//
+//  Created by Chloe Fortuna on 2/4/17.
+//  Copyright © 2017 Kobe-21. All rights reserved.
+//
+
+#ifndef FightOrFlight_Bridging_Header_h
+#define FightOrFlight_Bridging_Header_h
+
+
+#endif /* FightOrFlight_Bridging_Header_h */
