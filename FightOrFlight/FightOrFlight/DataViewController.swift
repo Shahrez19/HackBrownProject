b86097f590202b90317c9c7da22c0ee946273507
fortunac <cfortuna@bu.edu>
2017-02-04 14:39:17 -0500

    Create XCode project

---
+++ FightOrFlight/FightOrFlight/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  FightOrFlight
+//
+//  Created by Chloe Fortuna on 2/4/17.
+//  Copyright © 2017 Kobe-21. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
