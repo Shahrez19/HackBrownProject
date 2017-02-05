f262d76874c942ddb222c3a4416eb25115e99e0a
Jerry Liang <jeriscc@users.noreply.github.com>
2017-02-05 01:39:10 -0500

    more tweeks

--- extension/iframeInjector.js
+++ extension/iframeInjector.js
@@ -1,5 +1,5 @@
 var iFrame = document.createElement("iframe");
-iFrame.setAttribute("style", "position: fixed; z-index: 2000; width:80%; margin: 0 0 0 -40%;left:50%; padding:none; border-style: none");
+iFrame.setAttribute("style", "position: fixed; z-index: 2000; width:100%; height:100%; margin: 0 0 0 -40%;left:50%; padding: 0 0 0 0; border-style: none");
 iFrame.src = chrome.extension.getURL("embed.htm");
 
 document.body.insertBefore(iFrame, document.body.firstChild);
