ffd7c00ca3f0cc2d78b0840cbe62b7d582c470f9
Jerry Liang <jeriscc@users.noreply.github.com>
2017-02-05 06:21:21 -0500

    audio interface

--- extension/HelloWorld.js
+++ extension/HelloWorld.js
@@ -2,10 +2,9 @@ $(document).ready(jQueryMain);
 
 function jQueryMain() {
     $("#bttn").click(function () { //use a class, since your ID gets mangled
-        $("body").addClass("animated fadeOut"); //add the class to the clicked element
+        $("body").removeClass("fadeIn").addClass("fadeOut"); //add the class to the clicked element
         setTimeout(function () {
             parent.postMessage('closeIframe', '*');
         }, 1000);
-
     });
 }
