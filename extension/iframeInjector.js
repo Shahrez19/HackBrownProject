e3896e244aa99be42cf51dba09bf9bba2e3aaff8
Jerry Liang <jeriscc@users.noreply.github.com>
2017-02-05 03:17:43 -0500

    ui-improvements

--- extension/iframeInjector.js
+++ extension/iframeInjector.js
@@ -1,5 +1,5 @@
 var iFrame = document.createElement("iframe");
-iFrame.setAttribute("style", "position: fixed; z-index: 2000; width:100%; height:100%; border-style: none");
+iFrame.setAttribute("style", "position: fixed; z-index: 2000; width:100%; height:100%; border-style: none;");
 iFrame.src = chrome.extension.getURL("embed.htm");
 
 document.body.insertBefore(iFrame, document.body.firstChild);
