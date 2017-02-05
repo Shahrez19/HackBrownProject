7782c0dca8f718283e418e2d8e8301911ca6f1bb
Jerry Liang <jeriscc@users.noreply.github.com>
2017-02-05 07:07:27 -0500

    UI improvements

--- extension/HelloWorld.js
+++ extension/HelloWorld.js
@@ -7,4 +7,39 @@ function jQueryMain() {
             parent.postMessage('closeIframe', '*');
         }, 1000);
     });
+    var myaudio = new Audio('Ocean%20Sound%20Effect.mp3');
+    myaudio.play();
+    myaudio.muted = true;
+    myaudio.loop = true;
+    $("#bttn2").click(function () {
+        if (myaudio.muted != true) {
+            myaudio.muted = true;
+            $("#icon").removeClass("fa-volume-up");
+            $("#icon").addClass("fa-volume-off");
+        } else {
+            myaudio.muted = false;
+            $("#icon").removeClass("fa-volume-off");
+            $("#icon").addClass("fa-volume-up");
+        }
+    });
+
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    var totalSeconds = 0;
+    setInterval(setTime, 1000);
+
+    function setTime() {
+        ++totalSeconds;
+        secondsLabel.innerHTML = pad(totalSeconds % 60);
+        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+    }
+
+    function pad(val) {
+        var valString = val + "";
+        if (valString.length < 2) {
+            return "0" + valString;
+        } else {
+            return valString;
+        }
+    }
 }
