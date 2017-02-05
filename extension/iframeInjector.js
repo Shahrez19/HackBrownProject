ee837bd795dc585e8d1fdc014b6d84ecd1a4fc1b
Jerry Liang <jeriscc@users.noreply.github.com>
2017-02-05 05:28:58 -0500

    Iframe generating code now inside function. More UI tweeks.

--- extension/iframeInjector.js
+++ extension/iframeInjector.js
@@ -2,9 +2,12 @@ var iFrame = document.createElement("iframe");
 iFrame.setAttribute("style", "position: fixed; z-index: 2000; width:100%; height:100%; border-style: none;");
 iFrame.src = chrome.extension.getURL("embed.htm");
 
-document.body.insertBefore(iFrame, document.body.firstChild);
+function createFrame() {
+    document.body.insertBefore(iFrame, document.body.firstChild);
+}
+createFrame();
 
-addEventListener('message', function(ev) {
+addEventListener('message', function (ev) {
     if (ev.data === 'closeIframe') {
         iFrame.parentNode.removeChild(iFrame); // Your code
     }
