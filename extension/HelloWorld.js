3dd32464d5ae807addb6876622365b4f0813c011
Jerry Liang <jeriscc@users.noreply.github.com>
2017-02-05 05:15:34 -0500

    further editted ui

--- extension/HelloWorld.js
+++ extension/HelloWorld.js
@@ -1,9 +1,11 @@
 $(document).ready(jQueryMain);
 
 function jQueryMain() {
-    $(".myApp").append('<p>I\'m Added by jQuery</p>');
     $("#bttn").click(function () { //use a class, since your ID gets mangled
-        $(".myApp").addClass("fadeOut"); //add the class to the clicked element
-        $('#iframe', window.parent.document).remove();
+        $("body").addClass("animated fadeOut"); //add the class to the clicked element
+        setTimeout(function () {
+            parent.postMessage('closeIframe', '*');
+        }, 1000);
+
     });
 }
--- extension/iframeInjector.js
+++ extension/iframeInjector.js
@@ -3,3 +3,9 @@ iFrame.setAttribute("style", "position: fixed; z-index: 2000; width:100%; height
 iFrame.src = chrome.extension.getURL("embed.htm");
 
 document.body.insertBefore(iFrame, document.body.firstChild);
+
+addEventListener('message', function(ev) {
+    if (ev.data === 'closeIframe') {
+        iFrame.parentNode.removeChild(iFrame); // Your code
+    }
+});
