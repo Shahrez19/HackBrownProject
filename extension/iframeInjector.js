6bc7791d692bae9d53fe39f6a2eb2f12679d77f1
Jerry Liang <jeriscc@users.noreply.github.com>
2017-02-05 10:12:10 -0500

    Fixed issues

--- extension/iframeInjector.js
+++ extension/iframeInjector.js
@@ -1,12 +1,7 @@
 var iFrame = document.createElement("iframe");
 iFrame.setAttribute("style", "position: fixed; z-index: 2000; width:100%; height:100%; border-style: none;");
 iFrame.src = chrome.extension.getURL("embed.htm");
-
-function createFrame() {
-    document.body.insertBefore(iFrame, document.body.firstChild);
-}
-createFrame();
-
+document.body.insertBefore(iFrame, document.body.firstChild);
 addEventListener('message', function (ev) {
     if (ev.data === 'closeIframe') {
         iFrame.parentNode.removeChild(iFrame); // Your code
