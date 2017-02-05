d6a63f03711da4c0a99b3ccbd5ffba3363b140c2
Jerry Liang <jeriscc@users.noreply.github.com>
2017-02-05 04:16:42 -0500

    stuff

--- extension/HelloWorld.js
+++ extension/HelloWorld.js
@@ -1,8 +1,9 @@
 $(document).ready(jQueryMain);
 
 function jQueryMain() {
-    $(".myApp").append('<p>Added by jQuery</p>');
+    $(".myApp").append('<p>I\'m Added by jQuery</p>');
     $("#bttn").click(function () { //use a class, since your ID gets mangled
         $(".myApp").addClass("fadeOut"); //add the class to the clicked element
+        $('#iframe', window.parent.document).remove();
     });
 }
